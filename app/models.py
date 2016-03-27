da0f005a01abe124d5ae090cef330d896b5b90e5
Kigawas <to.be.impressive@gmail.com>
2016-03-27 10:12:48 +0000

    Add scoring requirements

--- app/models.py
+++ app/models.py
@@ -20,4 +20,16 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
-#class 
+class Essay(db.Model):
+    __tablename__ = 'essays'
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text)
+    time = db.Column(db.DateTime(True))
+    score = db.Column(db.Float)
+    spell_errors = db.Column(db.Text)
+    grammar_errors = db.Column(db.Text)
+    
+    def __repr__(self):
+        return '<Essay {0}>: {1}. Created at:{2}'.format(self.text, self.score, self.time)
+    
+    
